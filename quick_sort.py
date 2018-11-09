91dd0496df9812e2b6156670faad7fa6846ee733
myself <myself@qq.com>
2018-11-09 22:40:07 +0800

    快速排序

---
+++ quick_sort.py
@@ -0,0 +1,31 @@
+def sub_sort(list1, low, height):
+    key = list1[low]
+    while low < height:
+            while low < height and list1[height] >= key:
+                height -= 1
+            while low < height and list1[height] < key:
+                list1[low] = list1[height]
+                low += 1
+                list1[height] = list1[low]
+    list1[low] = key
+    return low
+
+# #思路
+# （1）先从数列中取出一个数作为基准数。
+# （2）分区过程，将比这个数大的数全放到它的右边，小于或等于它的数全放到它的左边。
+# （3）再对左右区间重复第二步，直到各区间只有一个数。
+
+
+
+def quick_sort(list1, low, height):
+    if low < height:
+        index_key = sub_sort(list1, low, height)
+        quick_sort(list1, low, index_key)
+        quick_sort(list1, index_key+1, height)
+
+
+if __name__ == '__main__':
+    list1 = [5, 10, 88, 0, 99, 1, 0, 100, 53]
+    print(list1)
+    quick_sort(list1, 0, len(list1)-1)
+    print(list1)
--- wechat_person_server.py
+++ wechat_person_server.py
@@ -5,7 +5,7 @@ import itchat,time,re
 from itchat.content import *
 @itchat.msg_register([TEXT])
 def text_reply(msg):
-    for i in range(3):
+    for i in range(2):
         time.sleep(2)
         if "二狗" in msg["Text"]:
             itchat.send(("你才是二狗"),msg["FromUserName"])
@@ -16,7 +16,7 @@ def text_reply(msg):
         elif "在吗" in msg["Text"]:
             itchat.send(("请不要问我在不在，万一是找我借钱呢"), msg["FromUserName"])
         else:
-            itchat.send(("请发红包开启与机器人美女聊天模式"), msg["FromUserName"])
+            itchat.send(("然后呢?"), msg["FromUserName"])
 # match3 = re.search("资料", msg["Text"]).span()
 @itchat.msg_register([PICTURE,RECORDING,VIDEO,SHARING])
 def other_reply(msg):
